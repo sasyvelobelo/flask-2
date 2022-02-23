e7c52ba9a39db268c0b7512b1fa78b093b4ba36c
GabrieleGalvani <dilecce.gabriele@iisgalvanimi.edu.it>
2022-02-23 13:58:17 +0000

    3imm es

--- appES1.py
+++ appES1.py
@@ -1,8 +1,21 @@
 from flask import Flask, render_template
 app = Flask(__name__)
+import random 
+
 
 @app.route('/')
 def hello_world():
+    return render_template("esercizio1.html")
+
+@app.route('/')
+def random_number:
+    numero_casuale = random.randint(0,8)
+    if numero_casuale < 2:
+        previsione = "pioggia"
+    elif numero_casuale >3 & numero_casuale <5:
+        meteo = "nuvoloso"
+    else:
+        meteo = "sole"
     return render_template("esercizio1.html")
 
 
