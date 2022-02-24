4c67f290268c8f307f01f0da0fb1acf432f6760e
GabrieleGalvani <dilecce.gabriele@iisgalvanimi.edu.it>
2022-02-24 10:54:13 +0000

    es1 part 2

--- appES1.py
+++ appES1.py
@@ -7,16 +7,19 @@ import random
 def hello_world():
     return render_template("esercizio1.html")
 
-@app.route('/')
-def random_number:
+@app.route('/meteo')
+def random_number():
     numero_casuale = random.randint(0,8)
     if numero_casuale < 2:
-        previsione = "pioggia"
+        meteo1 = "pioggia"
+        immagine ='static/pioggia.jpg'
     elif numero_casuale >3 & numero_casuale <5:
-        meteo = "nuvoloso"
+        meteo1 = "nuvoloso"
+        immagine ='static/nuvoloso.jpg'
     else:
-        meteo = "sole"
-    return render_template("esercizio1.html")
+        meteo1 = "sole"
+        immagine ='static/soleggiato.jpg'
+    return render_template("meteo.html",meteo = meteo1,immagine1 = immagine )
 
 
 
