f972c0d213cd08c680a930aac9ce7a3b33f05c0c
GabrieleGalvani <dilecce.gabriele@iisgalvanimi.edu.it>
2022-03-09 13:29:38 +0000

    es2 finito

--- appesercizio2.py
+++ appesercizio2.py
@@ -1,38 +1,55 @@
-#realizzare un sito web che permetta la registrazione degli utenti, l'utente inserisce il nome, uno username, una password, la conferma della password ed il sesso
-#se le informazioni sono corrette il sito salva le informazoni in una struttura dati opportuna(lista di dizionari)
-#prevedere la possibilità di fare il login inserendo username e password, se sono corrette fornire un messaggio di benvenuto diverso a secondo del sesso
+# Realizzare un sito web che permetta la registrazione degli utenti
+# L'utente inserisce il nome, uno username, una password
+# La conferma della password e il sesso.
+# Se le informazioni sono corrette il sito salva le informazioni in una struttura dati opportuna (lista di dizionari)
 
-from flask import Flask, render_template,request
-utenti = []
+# Prevedere la possibilità di fare il login 
+# Se sono corrette fornire un messaggio di benvenuto diverso a seconda del sesso
 
+from flask import Flask, render_template, request
 app = Flask(__name__)
 
+lista = []
 
 @app.route('/', methods=['GET'])
-def hello_world():
-    return render_template("form3.html")
-    
+def home():
+    return render_template('form3.html')
 
 @app.route('/data', methods=['GET'])
 def data():
-    Name = request.args["Name"]
-    username = request.args["Username"]
-    password = request.args["Password"]
-    conferma_password = request.args["Conferma_Password"]
-    sesso =  request.args["Sex"]
-    if password == conferma_password:
-        if sesso == 'M':
-            msg = 'Benvenuto' + Name
-        else:
-            msg = 'Benvenuta' + Name
-        #inserimento nella lista degli utenti
-        utenti.append({"name": Name, "username": username, "password": password, "conferma_password": conferma_password, "sesso": sesso})
-        return render_template("welcome1.html",messaggio = msg)
-    else:
-        return ('Errore')
+  password = request.args['Password']
+  conferma_password = request.args['Conferma password']
+  sesso = request.args['Sex']
+  username = request.args['Username']
+  nome = request.args['Name']
+  if password == conferma_password:
+      utente = {'Nome': nome, 'Username': username, 'Sex': sesso, 'Password': password}
+      lista.append(utente)
+      print(lista)
+      if sesso == 'M':
+          saluto = 'Benvenuto'
+      elif sesso == 'F':
+          saluto = 'Benvenuta'
+      else:
+          saluto = 'Benvenut*'
+      return render_template('login.html', benvenuto = saluto)
+  else:
+      return render_template('Errore.html')
 
-
-
+@app.route('/login', methods=['GET'])
+def login():
+    username_log = request.args['Username']
+    password_log = request.args['Password']
+    for utent in lista:
+        if utent['Username'] == username_log and utent['Password'] == password_log:
+            if utent['Sex'] == 'M':
+                saluto = 'Benvenuto'
+            elif utent['Sex'] == 'F':
+                saluto = 'Benvenuta'
+            else:
+                saluto = 'Benvenut*'
+            return render_template('welcome.html', benvenuto = saluto, nome_user = username_log)
+    return render_template('Errore.html')
 
 if __name__ == '__main__':
   app.run(host='0.0.0.0', port=3245, debug=True) 
---
+++ appesercizio3.py
@@ -0,0 +1,6 @@
+#per realizzare un server web che permetta di conoscere capoluoghi di region. 
+#L'utente inserisce il nome della regione ed il programma restituisce il nome del capoluogo di regione.
+#caricare i capoluoghi di regioni in una opportuna struttura dati.
+
+#una volta fatto ciò, modificare poi l'es precedente per permettere all'utente di inserire un capuoluogo e di avere la regione in cui si trova.
+#l'utente scegli se avere la regione o il capuoluogo selezionando un radiobutton
