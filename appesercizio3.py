beffd68d307bd53c820d71214f3733ab5fb01cbc
GabrieleGalvani <dilecce.gabriele@iisgalvanimi.edu.it>
2022-03-13 14:18:48 +0000

    Es 3 regioni e capoluoghi

--- appesercizio3.py
+++ appesercizio3.py
@@ -8,33 +8,41 @@
 from flask import Flask, render_template, request
 app = Flask(__name__)
 
+capoluoghiRegione = {'Abruzzo':'L\'Aquila', 'Basilicata':'Potenza', 'Calabria':'Catanzaro', 'Campania':'Napoli', 'Emilia-Romagna':'Bologna', 'Friuli-Venezia Giulia':'Trieste', 'Lazio':'Roma', 'Liguria':'Genova', 'Lombardia':'Milano', 'Marche':'Ancona', 'Molise':'Campobasso', 'Piemonte':'Torino', 'Puglia':'Bari', 'Sardegna':'Cagliari', 'Sicilia':'Palermo', 'Toscana':'Firenze', 'Trentino-Alto Adige':'Trento', 'Umbria':'Perugia', 'Valle dAosta':'Aosta', 'Veneto':'Venezia'}
 
 @app.route('/', methods=['GET'])
 def home():
-    return render_template('form4.html')
+    return render_template('homees3.html')
 
-@app.route('/scelta', methods=['GET'])
-def scelta():
-    scelta = request.args['Scelta']
+@app.route("/data", methods=["GET"])
+def data():
+    scelta = request.args["Scelta"]
+    if scelta == "R":
+        return render_template("regionees3.html")
+    else:
+        return render_template("capoluogoes3.html")
 
 @app.route('/regione', methods=['GET'])
 def regioni():
-    capoluoghiRegione = {'Abruzzo':'L\'Aquila', 'Basilicata':'Potenza', 'Calabria':'Catanzaro', 'Campania':'Napoli', 'Emilia-Romagna':'Bologna', 'Friuli-Venezia Giulia':'Trieste', 'Lazio':'Roma', 'Liguria':'Genova', 'Lombardia':'Milano', 'Marche':'Ancona', 'Molise':'Campobasso', 'Piemonte':'Torino', 'Puglia':'Bari', 'Sardegna':'Cagliari', 'Sicilia':'Palermo', 'Toscana':'Firenze', 'Trentino-Alto Adige':'Trento', 'Umbria':'Perugia', 'Valle dAosta':'Aosta', 'Veneto':'Venezia'}
-    items = list(capoluoghiRegione.items())
-    reg = request.args['Regione']
-    for item in items:
-        if item[0]==reg:
-            r = item[1]
-        else:
-            return render_template('Errore.html')
-    return r
+    regione = request.args['Regione']
+    for key, value in capoluoghiRegione.items():
+        if regione == key:
+            capoluogo = value
+            return render_template('regionees3.html', risposta = capoluogo)
+    return render_template('erroreregione.html')
 
+@app.route('/capoluoghi', methods=['GET'])
+def capoluoghi():
+    capoluogo = request.args['Capoluogo']
+    for key, value in capoluoghiRegione.items():
+        if capoluogo == value:
+            regione = key
+            return render_template('capoluogoes3.html', risposta = regione)
+    return render_template('errorecapoluogo.html')
 
 
 if __name__ == '__main__':
-  app.run(host='0.0.0.0', port=3245, debug=True) 
-
-
+  app.run(host='0.0.0.0', port=3245, debug=True)
 
 
 
